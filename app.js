732e8d055a8c127f24bbe57b51b8a9642ade190a
Jacky.L <liujiaqiid@gmail.com>
2016-08-04 19:56:04 +0800

    [Init] add cors middleware and fix bug;

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var jwtmw = require('express-jwt');
+var corsmw = require('./src/helpers/middlewares/CorsHandler');
 var bodyParser = require('body-parser');
 var config = require('./config/server');
 var routers = require('./src/router');
@@ -10,6 +11,8 @@ var app = module.exports = express();
 app.use(bodyParser.json());
 /** TODO NOT Genaral  Authorization: Bearer <token> **/
 app.use(jwtmw({secret: new Buffer(config.SERVER_SECRET)}).unless({path: config.PUBLIC_PATHS}));
+/** CORS **/
+app.use(corsmw());
 /** Load routers **/
 routers(app);
 
---
+++ src/helpers/middlewares/CorsHandler.js
@@ -0,0 +1,15 @@
+module.exports = function(options){
+  console.log("beging......");
+  return function (req, res, next){
+    console.log("loading cros middleware");
+    // 监听res回调header方法
+    //res.on('header', function () {
+      console.log("on header call");
+      // 更新client session
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Client-Session');
+      res.setHeader('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    //});
+    next();
+  }
+}
